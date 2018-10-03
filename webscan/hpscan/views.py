4666c0f80a0c5f6cd915e18fb34edc0fa68ffc75
tum0xa <timofey.samodurov@mail.ru>
2018-10-03 22:23:50 +0500

    Added template index.html and static files (css, js)

--- webscan/hpscan/views.py
+++ webscan/hpscan/views.py
@@ -1,12 +1,14 @@
 import os
-
+from .tools import Scanner
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-
+def index(request):
+    return render(request, 'hpscan/index.html')
 
 # Create your views here.
 def scan(request):
-
+    options = request.GET.dict()
+    scan_process = Scanner(options)
+    scan_process.start()
     return HttpResponse("OK")
--- webscan/webscan/urls.py
+++ webscan/webscan/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hpscan/', include('hpscan.urls')),
+    path('hpscan', include('hpscan.urls')),
 ]
